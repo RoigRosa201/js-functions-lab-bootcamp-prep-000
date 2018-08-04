0bb987866f45746d9c72762103f302424f0c4eb0
Roig Rosa <roigrosa88@gmail.com>
2018-08-04 05:58:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function happyHolidays()
   return "Happy holidays!"
 }
 
+<<<<<<< HEAD
 function happyHolidaysTo(name) {
   return  `Happy holidays, ${name}!`
 }
@@ -17,3 +18,17 @@ function holidayCountdown(holiday, days)
  {
    return `It's ${days} days until ${holiday}!`
  }
+=======
+function happyHolidaysTo(Luis){
+  return  `Happy holidays, ${Luis}!`
+}
+
+function happyHolidayTo(thanksgiving,Luis){
+  return `Happy ${thanksgiving}, ${Luis}!`
+
+}
+
+function holidayCountdown (thanksgiving,5 daysleft){
+   return `it's ${5 days until}, ${thanksgiving}!`
+}
+>>>>>>> ec8f050e9f5a5c5f76ef4aee1457a2cfc58d5910
